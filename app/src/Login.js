a55fe7e5bdcae992d9959a748e8630a375c56948
Gabe Foster <afoste53@gmail.com>
2020-11-07 18:34:02 -0500

    added bulma

---
+++ app/src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bulma/css/bulma.css'
+
+export default function Login(){
+    return(
+        <div class="hero">
+            <h1 class="title">Welcome to Virtual Gear Closet!</h1>
+        </div>
+    );
+}
